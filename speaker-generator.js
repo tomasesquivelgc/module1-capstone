3f9451f0ab409fa3a363fdd10a87c82cabd1ab0e
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-21 02:13:02 -0300

    person profile change
    proper images add
    speaker profile css change
    now displays in two rows while in desktop

--- speaker-generator.js
+++ speaker-generator.js
@@ -1,42 +1,42 @@
 const speakers = [
 
   {
-    image: './images/people/joe-cool.jpg',
+    image: './images/people/lily-chen-profile.jpg',
     name: 'Lily Chen',
     position: 'Music producer and sound designer',
     description: 'Lily is a highly respected producer and sound designer who has worked with some of the biggest names in electronic music. She will share her insights on creating powerful, innovative sounds using the latest software and hardware',
   },
 
   {
-    image: './images/people/joe-cool.jpg',
+    image: './images/people/maxime-profile.jpg',
     name: 'Maxime Dubois',
     position: 'Synthesizer expert and historian',
     description: 'Maxime is a renowned expert on vintage synthesizers and the history of electronic music. He will give a talk on the evolution of the synthesizer and its impact on modern music.',
   },
 
   {
-    image: './images/people/joe-cool.jpg',
+    image: './images/people/zoe-profile.jpg',
     name: 'Zoe Ramirez',
     position: 'Live performance specialist',
     description: 'Zoe is an expert in live electronic music performance, having performed at major festivals around the world. She will share her insights on creating dynamic, engaging performances using live instruments, software, and effects.',
   },
 
   {
-    image: './images/people/joe-cool.jpg',
+    image: './images/people/jake-profile.jpg',
     name: 'Jake Patel',
     position: 'Music software engineer',
     description: 'Jake is a software engineer who has worked on some of the most popular music software and plugins in the industry. He will discuss the latest developments in music software and how they are changing the way we create and produce electronic music.',
   },
 
   {
-    image: './images/people/joe-cool.jpg',
+    image: './images/people/svetlana-profile.jpg',
     name: 'Svetlana Petrova',
     position: 'Experimental music composer',
     description: 'Svetlana is an experimental music composer who has explored the boundaries of electronic music in her work. She will discuss her unique approach to composition and offer insights on pushing the limits of the genre.',
   },
 
   {
-    image: './images/people/joe-cool.jpg',
+    image: './images/people/alexei-profile.jpg',
     name: 'Alexei Ivanov',
     position: 'Music journalist and historian',
     description: 'Alexei is a respected music journalist and historian who has covered electronic music for over two decades. He will share his extensive knowledge of the genre, its history, and its future, and offer insights on how to stay up-to-date with the latest trends and developments.',
