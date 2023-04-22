84e9a1fd105c9eca74a807335194527c863920db
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-22 16:13:24 -0300

    linters fix

--- see-more-button.js
+++ see-more-button.js
@@ -2,7 +2,7 @@ const seeMoreButton = document.getElementById('see-more-button');
 
 function seeMoreSpeakers() {
   const speakerSections = document.getElementsByClassName('hiding');
-  for (let i = 0; i < speakerSections.length; i++) {
+  for (let i = 0; i < speakerSections.length; i += 1) {
     if (speakerSections[i].style.display === 'flex' || speakerSections[i].style.display === '') {
       speakerSections[i].style.display = 'none';
     } else {
